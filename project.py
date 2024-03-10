f9a6c4e847d37f415b0022a0c3c004a1a16957bf
Rony2603 <rounakbandyopadhyay@gmail.com>
2024-03-10 23:39:52 +0530

    Code add

--- project.py
+++ project.py
@@ -1,3 +1,9 @@
 def deposit():
-    a = int(input("Enter your deposit:"))
-    
+    while True:
+        amount = input("Enter your deposit amount: $")
+        if amount.isdigit():
+            amount = int(input())
+        else:
+            print("Enter a number: ")
+    return amount
+deposit()
